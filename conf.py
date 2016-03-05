91ff69892d58393d271170b8a43f3e0aa76a136c
´William <will@artcontrolme>
2016-03-06 04:46:30 +1300

    bootstrap theme

--- conf.py
+++ conf.py
@@ -134,7 +134,7 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "srcco.de"
+THEME = "ipython"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
